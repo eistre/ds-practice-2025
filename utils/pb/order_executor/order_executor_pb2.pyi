1c7b78fa47dde3cbc66bcc7c1326664f0cb111f2
Taavi Eistre <taavi30eistre@gmail.com>
2025-04-06 20:46:37 +0300

    Add order executor service with gRPC implementation and leader election

---
+++ utils/pb/order_executor/order_executor_pb2.pyi
@@ -0,0 +1,12 @@
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ExecutorResponse(_message.Message):
+    __slots__ = ("ok",)
+    OK_FIELD_NUMBER: _ClassVar[int]
+    ok: bool
+    def __init__(self, ok: bool = ...) -> None: ...
