f28315a1366e84c8cd72046202077e954d5da47b
MarilinA <marilin.ahvenainen@ut.ee>
2025-05-26 20:58:31 +0300

    monitoring

--- payment/src/app.py
+++ payment/src/app.py
@@ -14,41 +14,81 @@ from payment.payment_pb2_grpc import *
 from payment import payment_pb2_grpc
 from payment import payment_pb2
 
+from opentelemetry import trace, metrics
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http.metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+
+resource = Resource.create(attributes={"service.name": "payment-service"})
+
+# Traces
+trace.set_tracer_provider(TracerProvider(resource=resource))
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://observability:4318/v1/traces"))
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+# Metrics
+reader = PeriodicExportingMetricReader(OTLPMetricExporter(endpoint="http://observability:4318/v1/metrics"))
+metrics.set_meter_provider(MeterProvider(resource=resource, metric_readers=[reader]))
+meter = metrics.get_meter(__name__)
+
+# Metric Instruments
+payment_counter = meter.create_counter("payment_requests", description="Total payment attempts")
+success_counter = meter.create_counter("payment_success", description="Total successful payments")
+abort_counter = meter.create_counter("payment_failed", description="Total failed payments")
+
 import logging
 logger = logging.getLogger()
 
 class PaymentService(payment_pb2_grpc.PaymentService):
     def __init__(self):
         self.prepared_payments = {}
+        PaymentService._instance = self
     
     def Prepare(self, request, context):
-        # Dummy version: if amount is small then approved (person likely has enough to pay) with higher cost 70% approval
-        approved = request.amount < 100 or random.random()>0.3
-        if approved:
-            self.prepared_payments[request.order_id] = request.amount
-            print(f"Payment: Prepared for order {request.order_id} with amount ${request.amount}")
-            return payment_pb2.PrepareResponse(ready=True)
+        with tracer.start_as_current_span("Prepare Payment") as span:
+            span.set_attribute("order_id", request.order_id)
+            span.set_attribute("amount", request.amount)
+            payment_counter.add(1)
+            # Dummy version: if amount is small then approved (person likely has enough to pay) with higher cost 70% approval
+            approved = request.amount < 100 or random.random()>0.3
+            if approved:
+                self.prepared_payments[request.order_id] = request.amount
+                print(f"Payment: Prepared for order {request.order_id} with amount ${request.amount}")
+                return payment_pb2.PrepareResponse(ready=True)
 
-        print(f"Payment: Rejected payment for order {request.order_id}, amount ${request.amount}")
-        return payment_pb2.PrepareResponse(ready=False)
+            print(f"Payment: Rejected payment for order {request.order_id}, amount ${request.amount}")
+            return payment_pb2.PrepareResponse(ready=False)
     
     def Commit(self, request, context):
-        if request.order_id in self.prepared_payments:
-            print(f"Payment: Payment committed for order {request.order_id}, amount ${self.prepared_payments[request.order_id]}")
-            del self.prepared_payments[request.order_id]
-            return payment_pb2.CommitResponse(success=True)
-        
-        print(f"Payment: Commit failed, no prepared payment for order {request.order_id}")
-        return payment_pb2.CommitResponse(success=False)
+        with tracer.start_as_current_span("Commit Payment") as span:
+            span.set_attribute("order_id", request.order_id)
+            span.set_attribute("amount", request.amount)
+            success_counter.add(1)
+            if request.order_id in self.prepared_payments:
+                print(f"Payment: Payment committed for order {request.order_id}, amount ${self.prepared_payments[request.order_id]}")
+                del self.prepared_payments[request.order_id]
+                return payment_pb2.CommitResponse(success=True)
+            
+            print(f"Payment: Commit failed, no prepared payment for order {request.order_id}")
+            return payment_pb2.CommitResponse(success=False)
     
     def Abort(self, request, context):
-        if request.order_id in self.prepared_payments:
-            del self.prepared_payments[request.order_id]
-            print(f"Payment: Payment aborted for order {request.order_id}")
-            return payment_pb2.AbortResponse(aborted=True)
+        with tracer.start_as_current_span("Abort Payment") as span:
+            span.set_attribute("order_id", request.order_id)
+            span.set_attribute("amount", request.amount)
+            abort_counter.add(1)
+            if request.order_id in self.prepared_payments:
+                del self.prepared_payments[request.order_id]
+                print(f"Payment: Payment aborted for order {request.order_id}")
+                return payment_pb2.AbortResponse(aborted=True)
 
-        print(f"Payment: Abort called, but no payment to abort for order {request.order_id}")
-        return payment_pb2.AbortResponse(aborted=False)
+            print(f"Payment: Abort called, but no payment to abort for order {request.order_id}")
+            return payment_pb2.AbortResponse(aborted=False)
     
 def serve():
     # Create a gRPC server
